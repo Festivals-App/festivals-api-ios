f0c937f8c935cbac46cc9191584b32c2b68d026a
Simon Gaus <phisto05@gmail.com>
2020-04-11 05:54:58 +0200

    Initial Commit

---
+++ EventusAPI-Swift/EventusAPI_Swift.h
@@ -0,0 +1,19 @@
+//
+//  EventusAPI_Swift.h
+//  EventusAPI-Swift
+//
+//  Created by Simon Gaus on 11.04.20.
+//  Copyright © 2020 Simon Gaus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EventusAPI_Swift.
+FOUNDATION_EXPORT double EventusAPI_SwiftVersionNumber;
+
+//! Project version string for EventusAPI_Swift.
+FOUNDATION_EXPORT const unsigned char EventusAPI_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventusAPI_Swift/PublicHeader.h>
+
+
