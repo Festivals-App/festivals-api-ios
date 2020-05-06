7bca5cd43cf8f78cafaa46820fbfc125f4e83c6a
Simon Gaus <phisto05@gmail.com>
2020-05-06 04:17:00 +0200

    Add swift package manager support

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "EventusAPI-Swift",
+    name: "EventusAPI",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "EventusAPI-Swift",
-            targets: ["EventusAPI-Swift"]),
+            name: "EventusAPI",
+            targets: ["EventusAPI"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "EventusAPI-Swift",
+            name: "EventusAPI",
             dependencies: []),
         .testTarget(
-            name: "EventusAPI-SwiftTests",
-            dependencies: ["EventusAPI-Swift"]),
+            name: "EventusAPITests",
+            dependencies: ["EventusAPI"]),
     ]
 )
