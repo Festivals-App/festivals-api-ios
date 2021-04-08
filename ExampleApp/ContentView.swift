fa6ad19c72911a862eec49de653517d564f2b3e9
Simon Gaus <phisto05@gmail.com>
2021-04-08 04:30:18 +0200

    Reorganize project

---
+++ ExampleApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FestivalsAPI
+//
+//  Created by Simon Gaus on 08.04.21.
+//  Copyright © 2021 Simon Gaus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleApp/FestivalsAPIApp.swift
@@ -0,0 +1,18 @@
+//
+//  FestivalsAPIApp.swift
+//  FestivalsAPI
+//
+//  Created by Simon Gaus on 08.04.21.
+//  Copyright © 2021 Simon Gaus. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct FestivalsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
